c8840b0a2369b8676ca49e4530a5ba40077c6072
razeo <razeo9487@gmail.com>
2025-05-03 18:54:58 +0800

    add FireBase add regist page change itemcardbackground change login & regist backround round change login & regist page design

--- app/src/main/java/com/example/swipecard/registActivity.java
+++ app/src/main/java/com/example/swipecard/registActivity.java
@@ -1,10 +1,10 @@
 package com.example.swipecard;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -14,32 +14,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-
-public class loginActivity extends AppCompatActivity {
-    ImageView mImageView;
-    Button  mButtonSignup, mButtonLogin;
-    EditText mUsername,mPassword;
+public class registActivity extends AppCompatActivity {
+    Button mbacklog,mregist;
+    EditText mregEmail,mregpassword;
     FirebaseAuth auth;
     FirebaseAuth.AuthStateListener authStateListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_regist);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        mImageView = findViewById(R.id.imageView);
-        mUsername = findViewById(R.id.editTextEmail);
-        mPassword = findViewById(R.id.editTextNumberPassword);
+        mregEmail = findViewById(R.id.editTextEmail2);
+        mregpassword = findViewById(R.id.editTextNumberPassword2);
+
         auth = FirebaseAuth.getInstance();
         authStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -47,33 +45,30 @@ public class loginActivity extends AppCompatActivity {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
             }
         };
-
-        mButtonSignup = findViewById(R.id.button5);
-        mButtonSignup.setOnClickListener(new View.OnClickListener() {
+        mbacklog = findViewById(R.id.backloginbutton);
+        mbacklog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(loginActivity.this,registActivity.class);
+                Intent intent = new Intent(registActivity.this, loginActivity.class);
                 startActivity(intent);
             }
         });
-
-        mButtonLogin = findViewById(R.id.button6);
-        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+        mregist = findViewById(R.id.regbutton);
+        mregist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String logemail = mUsername.getText().toString();
-                String logpsw = mPassword.getText().toString();
-                auth.signInWithEmailAndPassword(logemail,logpsw).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                String regemail = mregEmail.getText().toString();
+                String regpsw = mregpassword.getText().toString();
+                auth.createUserWithEmailAndPassword(regemail,regpsw).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
-                    public void onSuccess(AuthResult authResult) {
-                        Toast.makeText(loginActivity.this,"登入成功！",Toast.LENGTH_LONG).show();
-                        Intent intent = new Intent(loginActivity.this,MainActivity.class);
-                        startActivity(intent);
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(loginActivity.this,"請確認帳號密碼是否正確！",Toast.LENGTH_LONG).show();
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isComplete()){
+                            Toast.makeText(registActivity.this,"註冊成功",Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(registActivity.this, loginActivity.class);
+                            startActivity(intent);
+                        }else{
+                            Toast.makeText(registActivity.this,"註冊失敗",Toast.LENGTH_LONG).show();
+                        }
                     }
                 });
             }
